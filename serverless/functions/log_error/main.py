900c6c6cbf9cc12de5dbaa572ba3aab1287b18b2
Jordan Mance <mancej@snagajob.com>
2020-07-04 11:40:15 -0400

    Adding 'version' to reported errors. Sending error counts to cloud-watch metrics as well

--- serverless/functions/log_error/main.py
+++ serverless/functions/log_error/main.py
@@ -1,5 +1,6 @@
 import json
 import boto3
+from aws_embedded_metrics import metric_scope
 
 from data.dao.ssmdao import SsmDao
 from utils.logging import LoggingUtils
@@ -12,10 +13,20 @@ ssm = SsmDao(boto3.client('ssm'))
 STACKTRACE = 'stacktrace'
 OS = 'os'
 COMMAND = 'command'
+VERSION = 'version'
 ERROR_TOPIC_SSM_KEY = '/figgy/resources/sns/error-topic/arn'
 REQUIRED_PROPERTIES = [STACKTRACE, OS, COMMAND]
 
 ERROR_TOPIC_ARN = ssm.get_parameter(ERROR_TOPIC_SSM_KEY)
+
+
+@metric_scope
+def publish_error_metric(platform: str, version: str, metrics):
+    metrics.set_namespace("figgy")
+    metrics.put_dimensions({"Version": version, "Platform": platform})
+
+    metrics.set_namespace("figgy")
+    metrics.put_metric('error', 1, "Count")
 
 
 def handle(event, context):
@@ -23,13 +34,16 @@ def handle(event, context):
     log.info(f'Got request with body: {body}')
     stacktrace = body.get(STACKTRACE)
     os = body.get(OS)
+    version = body.get(VERSION)
     command = body.get(COMMAND)
 
     Utils.validate(stacktrace and os and command, f"These JSON properties are required: {REQUIRED_PROPERTIES}")
 
+    publish_error_metric(platform=os, version=version)
+
     sns.publish(
         TopicArn=ERROR_TOPIC_ARN,
-        Message=f'OS: {os}\n\n Stacktrace:\n\n{stacktrace}',
+        Message=f'Version: {version}\nOS: {os}\n\n Stacktrace:\n\n{stacktrace}',
         Subject=f'Error detected for command: {command}'[:100]
     )
 
